353b6dedeeea112f72f4ace50d1b9ca5ba00b8fb
PieterNatanael <pieternatanael@icloud.com>
2024-05-14 13:45:35 +0800

    Initial Commit

---
+++ What Day?/What_Day_App.swift
@@ -0,0 +1,17 @@
+//
+//  What_Day_App.swift
+//  What Day?
+//
+//  Created by Pieter Yoshua Natanael on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct What_Day_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
