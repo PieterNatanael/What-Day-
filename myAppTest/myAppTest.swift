7d3d87e7dee273be4dfe9764c7c7361f2c3c65d0
PieterNatanael <pieternatanael@icloud.com>
2024-10-24 19:23:24 +0800

    Initial Commit

---
+++ myAppTest/myAppTest.swift
@@ -0,0 +1,16 @@
+//
+//  myAppTest.swift
+//  myAppTest
+//
+//  Created by Pieter Yoshua Natanael on 23/10/24.
+//
+
+import Testing
+
+struct myAppTest {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
